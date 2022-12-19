3443683610d89c089bbe9d77df2bca7c385e7a21
amauri <amauri.olisboa@gmail.com>
2022-12-19 15:41:09 +0000

    Commit exercice 8 Javascript

---
+++ 8_JS/javascript.js
@@ -0,0 +1,29 @@
+function selectedImageClicked(){
+  //GAME 1
+  document.getElementById("image1").style.borderWidth = '10px';
+  document.getElementById("image1").style.borderStyle = 'solid';
+  document.getElementById("image1").style.borderColor = 'lightgreen';
+  document.getElementById("image1").style.borderRadius = '10px';
+
+ console.log(imageSelected);
+}
+
+//GAME 2
+function selectedImageClicked2(){
+
+  document.getElementById("image2").style.borderWidth = '10px';
+  document.getElementById("image2").style.borderStyle = 'solid';
+  document.getElementById("image2").style.borderColor = 'lightgreen';
+  document.getElementById("image2").style.borderRadius = '10px';
+   console.log(imageSelected);
+}
+
+//GAME 3
+function selectedImageClicked3(){
+
+  document.getElementById("image3").style.borderWidth = '10px';
+  document.getElementById("image3").style.borderStyle = 'solid';
+  document.getElementById("image3").style.borderColor = 'lightgreen';
+  document.getElementById("image3").style.borderRadius = '10px';
+   console.log(imageSelected);
+}
