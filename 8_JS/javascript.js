5f06e405bf02886f25028a3eebf49a4ff2a0bd0d
amauri <amauri.olisboa@gmail.com>
2022-12-20 09:58:45 +0000

    exercice 9 JS soluction

--- 8_JS/javascript.js
+++ 8_JS/javascript.js
@@ -1,29 +1,26 @@
 function selectedImageClicked(){
   //GAME 1
-  document.getElementById("image1").style.borderWidth = '10px';
-  document.getElementById("image1").style.borderStyle = 'solid';
-  document.getElementById("image1").style.borderColor = 'lightgreen';
-  document.getElementById("image1").style.borderRadius = '10px';
+  document.getElementById("image1").style.border ="7px solid lightgreen";
 
+  document.getElementById("image2").style.border="";
+  document.getElementById("image3").style.border="";
  console.log(imageSelected);
 }
 
 //GAME 2
 function selectedImageClicked2(){
 
-  document.getElementById("image2").style.borderWidth = '10px';
-  document.getElementById("image2").style.borderStyle = 'solid';
-  document.getElementById("image2").style.borderColor = 'lightgreen';
-  document.getElementById("image2").style.borderRadius = '10px';
+  document.getElementById("image2").style.border ="7px solid lightgreen";
+  document.getElementById("image1").style.border="";
+  document.getElementById("image3").style.border="";
    console.log(imageSelected);
 }
 
 //GAME 3
 function selectedImageClicked3(){
 
-  document.getElementById("image3").style.borderWidth = '10px';
-  document.getElementById("image3").style.borderStyle = 'solid';
-  document.getElementById("image3").style.borderColor = 'lightgreen';
-  document.getElementById("image3").style.borderRadius = '10px';
+  document.getElementById("image3").style.border ="7px solid lightgreen";
+  document.getElementById("image1").style.border="";
+  document.getElementById("image2").style.border="";
    console.log(imageSelected);
 }
---
+++ 9_JS/javascript.js
@@ -0,0 +1,34 @@
+function defaultBlue(){
+  document.getElementById("image1").style.background="blue";
+  console.log(defaultBlue)
+}
+
+function clicked1(){
+  document.getElementById("image1").style.background="blue";
+  document.getElementById("image2").style.background="";
+  document.getElementById("image3").style.background="";
+}
+
+function clicked2(){
+document.getElementById("image2").style.background="blue";
+document.getElementById("image1").style.background="";
+document.getElementById("image3").style.background="";
+}
+
+function clicked3(){
+document.getElementById("image3").style.background="blue";
+document.getElementById("image1").style.background="";
+document.getElementById("image2").style.background="";
+}
+
+//Below is exercice 10 of worksheet 2
+
+function buttonOnClick() {
+  //using the value to get the info of the HTML input
+var name = document.getElementById("name").value;
+var price = document.getElementById("price").value;
+var email = document.getElementById("email").value;
+console.log(buttonOnClick);
+
+document.getElementById("output").innerHTML = "Hi, "+name+"! An invoice of "+price+" EUROS will be send to "+email+". Thank you for your order!";
+}
