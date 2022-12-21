490f54b1832b78e4390afc8a7eea46e5d1cf6285
amauri <amauri.olisboa@gmail.com>
2022-12-21 09:50:00 +0000

    commit exercice 15 with masculine feminine checkbox

--- 15_JS/javascript.js
+++ 15_JS/javascript.js
@@ -3,25 +3,32 @@
 function checkFilled(){
   var studentName = document.getElementById("studentName").value;
   var grade = document.getElementById("grade").value;
-  var genrer = document.getElementById("genrerCheckBox").checked;
 
-  if (studentName == 0 || grade == 0){
-    alert("you must put all the information");
-    }
-  else if(grade < 1){
-    alert("The grade must be a real number");
+
+  if (studentName == "" || grade.length == 0){
+    document.getElementById("errorOutput").innerHTML += "You must put all information.<br>";
   }
-  else if(grade < 1 || grade > 60){
-    alert("The grade must be between 01 and 60");
+
+  else if(parseInt(grade) < 1 || parseInt(grade) > 60){
+    document.getElementById("errorOutput").innerHTML += "The grade must be between 01 and 60.<br>";
   }
-  else if(studentName.length <= 3){
-    alert("The name must have at least 3 characteres");
+
+  else if(studentName.length < 3){
+    document.getElementById("errorOutput").innerHTML += "The name must have at least 3 characteres.<br>";
   }
+
   else if(isNaN(grade)){
-    alert("The grade must have just numbers");
+    document.getElementById("errorOutput").innerHTML += "The grade must have just numbers.<br>";
   }
 
-/*
+  else {
+    //add infos to the list using another function.
+    addDataToList();
+    //clear error messages.
+      document.getElementById("errorOutput").innerHTML="";
+  }
+
+  /*
   console.log(studentName);
   console.log(grade);
   console.log(genrer);
@@ -30,20 +37,44 @@ function checkFilled(){
 
 
 //create a new function to add the information at the list
+
 function addDataToList() {
   var studentNameData = document.getElementById("studentName").value;
   var gradeData = document.getElementById("grade").value;
-  var genrerData = document.getElementById("genrerCheckBox").checked;
+  var gradeGenrerData = document.getElementById("genrerCheckBox").checked;
 
+  /*this above IF check if the variable gradeGenrerData is masculine or feminine
+  if is clicked in masculine on the input radio form in html, it will return a TRUE value
+  if is clicked in feminine, it will return a FALSE value.
+  By this principle, we can save this information in a new variable that i called GENRER, and then do the if
+  to give the values of masculine and feminine.
+  */
+  var genrer;
+  if(gradeGenrerData == true){
+    genrer = "masculine";
+  }
+  else {
+    genrer = "feminine";
+  }
+
+
+  console.log(gradeGenrerData);
+  console.log(genrer);
   //add itens to the list
   //create a new variable -listElement- and make it search for the list with id #list in html.
   var listElement = document.getElementById("list");
   //creates a variable -listItem- and makes it create a new element with .createElement("li").
   var listItem = document.createElement("li");
+
   //modify the html with InnerHTML and gave it the information of studentNameData;
-  listItem.innerHTML = studentNameData;
+  listItem.innerHTML = studentNameData+" ("+gradeData+")";
   //add the listItem to children of listElement;
   listElement.appendChild(listItem);
 
 
+
+  /*
+  //this one also add itens to the list - this was the solution of the professor
+  document.getElementById("list").innerHTML +="<li>" + studentNameData + "(" + gradeData + ")</li>";
+ */
 }
